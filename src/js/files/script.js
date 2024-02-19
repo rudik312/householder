c017a0c450cef4ff25d7a1a8853cd8a818c3e0b7
rudik312 <fl.rudik@mail.ru>
2024-02-19 23:48:00 +0700

    add section indispensable

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -6,14 +6,15 @@ import { flsModules } from "./modules.js";
 const myScroll = new LocomotiveScroll({
   el: document.querySelector("[data-scroll-container]"),
   smooth: true,
-  offset: [0, 0],
+  // offset: [0, 0],
   // repeat: false,
 
-  direction: "vertical", // or horizontal
-  lerp: 0.05, // inertia
-  touchMultiplier: 2,
 
-  class: "is-inview",
+  // direction: "vertical", // or horizontal
+  lerp: 0.05, // inertia
+  // touchMultiplier: 2,
+
+  // class: "is-inview",
   // initClass: 'has-scroll-init',
   // scrollbarClass: 'c-scrollbar',
   // scrollingClass: 'has-scroll-scrolling',
@@ -29,5 +30,22 @@ const myScroll = new LocomotiveScroll({
   // gestureDirection: "vertical", // or horizontal, both
   // reloadOnContextChange: false,
   // resetNativeScroll: true,
-
+  offset: document.querySelector('.install__heading').offsetHeight
 });
+
+
+// как установить срабатывание scroll, когда секция начинается
+
+// const section = document.querySelector('.install__parallax'); // выбираем секцию, которую нужно отслеживать
+
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//             // выполнить нужные действия, когда секция появляется на экране
+//            
+//         }
+//     });
+// });
+
+// observer.observe(section);
+
