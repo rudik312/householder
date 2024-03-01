e917be307fdef752871c854d6cf3a3440038ebe6
rudik312 <fl.rudik@mail.ru>
2024-03-02 00:33:45 +0700

    add section video and trust

--- src/js/libs/gsap.js
+++ src/js/libs/gsap.js
@@ -2,63 +2,98 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger.js";
 import { ScrollSmoother } from "gsap/ScrollSmoother.min.js";
 
-
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 ScrollTrigger.normalizeScroll(true);
 
-document.addEventListener('DOMContentLoaded', function (event) {
-
-
-
-const smoother = ScrollSmoother.create({
+document.addEventListener("DOMContentLoaded", function (event) {
+  const smoother = ScrollSmoother.create({
     wrapper: ".wrapper",
     content: ".content",
-		smooth: 1.5,
-		effects: true,
-    normalizeScroll: true, 
+    smooth: 1.5,
+    effects: true,
+    normalizeScroll: true,
   });
 
   ScrollTrigger.create({
     trigger: ".install__content",
     pin: true,
     start: "top top",
-    end: "+=1000",
-    markers: true
+    end: "+=600",
+    scrub: true,
+    // markers: true,
+  });
+
+  gsap.to(".install__parallax", {
+    scrollTrigger: ".install__content", // start the animation when ".box" enters the viewport (once)
+    start: "10%",
+    end: "30%",
+    y: "-50%",
+    markers: true,
   });
 
 
-  ScrollTrigger.create({
-    trigger: ".install__parallax",
-    // pin: true,
-    // start: "bottom bottom",
-    start: "+=800",
-    // end: "+=950",
-    markers: true
-  });
-
-
-
-
-
-
-
-  // как установить срабатывание scroll, когда секция начинается
-
-  // const section = document.querySelector('.install__parallax'); // выбираем секцию, которую нужно отслеживать
-
-  // const observer = new IntersectionObserver((entries) => {
-  //     entries.forEach(entry => {
-  //         if (entry.isIntersecting) {
-  //             // выполнить нужные действия, когда секция появляется на экране
-  //
-  //         }
-  //     });
+  
+  // gsap.timeline({
+  //   scrollTrigger: {
+  //     trigger: ".install",
+  //     scrub: 1,
+  //     pin: '.install__parallax',
+  //     pinSpacing: true,
+  //     start: "10%",
+  //     end: "30%",
+  //     markers: true,
+  //   },
   // });
 
-  // observer.observe(section);
+  // ScrollTrigger.create({
+  //   trigger: ".install__parallax",
+  //   pin: true,
+  //   start: "top top",
+  //   end: "+=600",
+  //   scrub: true,
+  //   markers: true,
+  // });
 
 
-})
-  
 
+
+  // gsap.from('.draw', {
+  //   // drawSVG: "0%",
+  //   ease: "expo.out",
+  //   scrollTrigger: {
+  //     trigger: '.install__content',
+  //     // start: "clamp(top center)",
+  //     start: "bottom bottom",
+  //     scrub: true,
+  //     pin: '.install__parallax',
+  //     pinSpacing: false,
+  //     markers:true,
+  //   }
+  // })
+
+  // pin: '.install__container',
+  // pinSpacing: false,
+  // start: "top top",
+
+  // gsap.fromTo(".install__parallax", { top: '100px' }, {
+  //   top: '50px',
+  //   scrollTrigger: {
+	// 		trigger: '.install__parallax',
+	// 		start: 'top',
+	// 		end: '820',
+	// 		scrub: true
+	// 	}
+  // });
+
+  // ScrollTrigger.create({
+  //   trigger: ".install__parallax",
+  //   // pin: true,
+  //   // start: "bottom bottom",
+  //   start: "+500",
+  //   // end: "+=950",
+  //   // scrub: true,
+  //   markers: true,
+  // });
+
+});
