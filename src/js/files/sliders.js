36a1cd904e21616e03f1a1ade0bbba342757ac79
rudik312 <fl.rudik@mail.ru>
2024-03-24 23:29:20 +0700

    add section reviews

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -161,6 +161,83 @@ function initSliders() {
 		})
 	}
 
+  if (document.querySelector(".reviews__slider")) {
+    // Указываем склас нужного слайдера
+    // Создаем слайдер
+    new Swiper(".reviews__slider", {
+      // Указываем склас нужного слайдера
+      // Подключаем модули слайдера
+      // для конкретного случая
+      modules: [Navigation],
+      // direction: "vertical",
+      observer: true,
+      observeParents: true,
+      slidesPerView: 2.6,
+      spaceBetween: 20,
+      //autoHeight: true,
+      speed: 800,
+
+      //touchRatio: 0,
+      //simulateTouch: false,
+      loop: true,
+      //preloadImages: false,
+      //lazy: true,
+
+      // Эффекты
+      // effect: "slide",
+      // autoplay: {
+      //   delay: 2000,
+      //   // disableOnInteraction: false,
+      // },
+
+      // Пагинация
+      /*
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			},
+			*/
+
+      // Скроллбар
+      /*
+			scrollbar: {
+				el: '.swiper-scrollbar',
+				draggable: true,
+			},
+			*/
+
+      // Кнопки"влево/вправо"
+      navigation: {
+          prevEl: '.reviews__btn-prev',
+          nextEl: '.reviews__btn-next',
+      },
+      /*
+			// Брейкпоинты
+			breakpoints: {
+				640: {
+					slidesPerView: 1,
+					spaceBetween: 0,
+					autoHeight: true,
+				},
+				768: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+				992: {
+					slidesPerView: 3,
+					spaceBetween: 20,
+				},
+				1268: {
+					slidesPerView: 4,
+					spaceBetween: 30,
+				},
+			},
+			*/
+      // События
+      on: {},
+    });
+  }
+
 }
 // Скролл на базе слайдера (по классу swiper scroll для оболочки слайдера)
 function initSlidersScroll() {
